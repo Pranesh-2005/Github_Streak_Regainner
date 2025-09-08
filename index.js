35fc3388d8d0f360bafec6132a5f31b49093e439
Pranesh <praneshmadhan646@gmail.com>
2025-09-08 10:09:41 +0530

    changed index.js file

--- index.js
+++ index.js
@@ -3,17 +3,9 @@ import moment from "moment";
 import simpleGit from "simple-git";
 
 const path = "./dummy.txt";
-// Set date to 25th July 2025
-const targetDate = moment("2025-07-25").format();
 
-const makeCommits = (n) => {
-    if (n === 0) return simpleGit().push();
-    // Use the fixed date instead of random
-    const date = targetDate;
-    // Append a line to dummy.txt to create a change
-    fs.appendFileSync(path, `Commit on ${date}\n`);
-    console.log(date);
-    simpleGit().add([path]).commit(date, { "--date": date }, makeCommits.bind(this, --n));
-};
+const date = moment("2025-08-27").format();
 
-makeCommits(1);
+fs.appendFileSync(path, `Commit on ${date}\n`);
+console.log(date);
+simpleGit().add([path]).commit(date, { "--date": date }).push();
